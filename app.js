b2bd874dd61462bd215258dcb7b9f25e5a45921c
D0gan <faucheriefrancois@gmail.com>
2019-12-12 23:17:08 +0100

    ajout middleware json + encodage body / création méthode post / gestion des erreurs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const {success, error} = require('./src/tools');
+const bodyParser = require('body-parser');
 
 // morgan : outils de debug pour chaque resquest http
 app.use(morgan('dev'));
+
+// middleware pour utiliser le body en post
+app.use(bodyParser.json()); // parsing application/json
+app.use(bodyParser.urlencoded({extended:true})); // encodage du body
 
 const members = [
     {
@@ -22,28 +28,72 @@ const members = [
 
 app.get('/api', (req, res) =>{
     console.log("test");
-    res.send('Root API');
+    res.json('Root API');
 });
 
 app.get('/api/v1', (req, res) =>{
     console.log("V1");
-    res.send('API version 1');
+    res.json('API version 1');
 });
 
 app.get('/api/v1/cards/:id', (req, res) =>{
-    res.send(req.params);
+    res.json(success(req.params));
 });
 
 app.get('/api/v1/members/:id', (req, res) =>{
-    res.send(members[(req.params.id) - 1].name);
+    let index = getIndex(req.params.id);
+
+    if (typeof(index) == 'string'){
+        res.json(error(index))
+    }else{
+        res.json(success(members[index]));
+    }
 });
 
 app.get('/api/v1/members', (req, res) =>{
     if (req.query.max != undefined && req.query.max > 0){
-        res.send(members.slice(0,req.query.max));
+        res.json(success(members.slice(0,req.query.max)));
+    }
+    else if(req.query.max != undefined){
+        res.json(error("Wrong max value"));
+    }
+    else{
+        res.json(success(members));
+    }
+});
+
+app.post('/api/v1/members', (req, res) => {
+    if (req.body.name){
+        let sameName = false;
+
+        members.forEach(member => {
+            if (member.name == req.body.name){
+                sameName = true;
+            }
+        })
+
+        if (sameName){
+            res.json(error("name already taken"));
+        }else{
+            let member = {
+                id: members.length + 1,
+                name: req.body.name
+            };
+    
+            members.push(member);
+            res.json(success(member));
+        }        
     }else{
-        res.send(members);
+        res.json(error('no name value'));
     }
 });
 
 app.listen(8080, () => console.log("Started on port 8080"));
+
+const getIndex = id => {
+    for (let i = 0; i < members.length; i++){
+        if (members[i].id == id)
+            return i;
+    }
+    return 'Wrong id';
+}
